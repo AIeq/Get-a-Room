ed6f3767a6e571963f70a4ee1095ea533d51b2b1
Pietari Kokko <pietari.kokko@gmail.com>
2015-03-02 22:05:53 +0200

    Added js code

--- WWWService/WWWService/settings.py
+++ WWWService/WWWService/settings.py
@@ -75,6 +75,8 @@ STATIC_URL = '/static/'
 # Additional locations of static files
 STATICFILES_DIRS = (
     os.path.join(STATIC_ROOT, 'css'),
+    os.path.join(STATIC_ROOT, 'js'),
+    os.path.join(STATIC_ROOT, 'graphics'),
     os.path.join(STATIC_ROOT, 'bootstrap-3.3.2-dist'),
     os.path.join(STATIC_ROOT, 'bootstrap-3.3.2-dist', 'css'),
     os.path.join(STATIC_ROOT, 'bootstrap-3.3.2-dist', 'js'),
