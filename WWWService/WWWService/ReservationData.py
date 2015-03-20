3b6817b53fa640dd5361d4e3791c5c84b39e0527
Pietari Kokko <pietari.kokko@gmail.com>
2015-03-20 12:54:45 +0200

    Added option to reserve rooms in advance

--- WWWService/WWWService/ReservationData.py
+++ WWWService/WWWService/ReservationData.py
@@ -2,7 +2,12 @@
 
 from copy import deepcopy
 
-emptyWeek = {'thisWeek': [
+emptyRoom = {'nextWeek': [
+    ['','','','','','','','','','','','',],['','','','','','','','','','','','',],
+    ['','','','','','','','','','','','',],['','','','','','','','','','','','',],
+    ['','','','','','','','','','','','',],['','','','','','','','','','','','',],
+    ['','','','','','','','','','','','',],],
+    'thisWeek': [
     ['','','','','','','','','','','','',],['','','','','','','','','','','','',],
     ['','','','','','','','','','','','',],['','','','','','','','','','','','',],
     ['','','','','','','','','','','','',],['','','','','','','','','','','','',],
@@ -19,12 +24,12 @@ emptyWeek = {'thisWeek': [
     [0,0,0,0,0,0,0,0,0,0,0,0,],],}
  
 data = {
-  '142a':deepcopy(emptyWeek),
-  '142b':deepcopy(emptyWeek),
-  '143a':deepcopy(emptyWeek),
-  '143b':deepcopy(emptyWeek),
-  '243a':deepcopy(emptyWeek),
-  '243b':deepcopy(emptyWeek),
+  '142a':deepcopy(emptyRoom),
+  '142b':deepcopy(emptyRoom),
+  '143a':deepcopy(emptyRoom),
+  '143b':deepcopy(emptyRoom),
+  '243a':deepcopy(emptyRoom),
+  '243b':deepcopy(emptyRoom),
   }
 
 buildings = {'': deepcopy(data), 'Kirjasto': deepcopy(data), 'Maarintalo': deepcopy(data)}
@@ -32,17 +37,48 @@ buildings = {'': deepcopy(data), 'Kirjasto': deepcopy(data), 'Maarintalo': deepc
 def GetReservationData(building, room):
   "This queries reservation database and returns entries for a room in a building"
   "The first version will just return static list"
-  return buildings[building][room]
+  return buildings[building][room]['thisWeek']
+  
+def GetStatistics(building, room):
+  d = buildings[building][room]
+  statistics = deepcopy(d['statistics'])
+  for lastWeek in d['lastWeek']:
+    for timeSlot, _ in enumerate(lastWeek):
+      statistics[timeSlot] += int(lastWeek[timeSlot] != '')
+  return statistics
 
-def ReserveRoom(building, room, day, timeslot, email):
+def ReserveRoom(building, room, weekday, timeslot, email):
   "Timeslot is 0-12, 0 means from 8 to 9 am"
-  buildings[building][room]['thisWeek'][day][timeslot] = email
+  day = buildings[building][room]['thisWeek'][weekday]
+  if day[timeslot] == '':
+    day[timeslot] = email
+    return True
+  else:
+    return False
+
+def ReleaseRoom(building, room, weekday, timeslot):
+  "Timeslot is 0-12, 0 means from 8 to 9 am"
+  buildings[building][room]['thisWeek'][weekday][timeslot] = ''
+  
+def ReserveRoomAdvance(building, room, weekday, timeslot, email):
+  "Timeslot is 0-12, 0 means from 8 to 9 am"
+  day = buildings[building][room]['nextWeek'][weekday]
+  if day[timeslot] == '':
+    day[timeslot] = email
+    return True
+  else:
+    return False
+
+def ReleaseRoomAdvance(building, room, weekday, timeslot):
+  "Timeslot is 0-12, 0 means from 8 to 9 am"
+  buildings[building][room]['nextWeek'][weekday][timeslot] = ''
 
 def EndWeek(building):
     for roomName, d in buildings[building].iteritems():
-        for thisWeek, lastWeek, statistics in zip(d['thisWeek'], d['lastWeek'], d['statistics']):
+        for nextWeek, thisWeek, lastWeek, statistics in zip(d['nextWeek'], d['thisWeek'], d['lastWeek'], d['statistics']):
             for timeSlot, _ in enumerate(thisWeek):
                 statistics[timeSlot] += int(lastWeek[timeSlot] != '')
                 lastWeek[timeSlot] = thisWeek[timeSlot]
-                thisWeek[timeSlot] = ''
+                thisWeek[timeSlot] = nextWeek[timeSlot]
+                nextWeek[timeSlot] = ''
   
