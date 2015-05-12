39fc72de7acfe14c0ea0c33a4a4ebe590d1338ce
AIeq <pietari.kokko@aalto.fi>
2015-05-12 11:06:25 +0300

    Update Emails.py

--- WWWService/WWWService/Emails.py
+++ WWWService/WWWService/Emails.py
@@ -2,7 +2,7 @@
 from django.core.mail import EmailMessage
 from rooms.models import Email
 import ReservationData
-import serverAddress
+from serverAddress import serverAddress
 import sys
 
 emailHeader = \
